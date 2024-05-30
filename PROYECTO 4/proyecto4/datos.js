d81c2d4f960c1ea732739d534aa55249ae316477
paaardo <ikerpardo53@gmail.com>
2024-05-30 21:31:21 +0200

    feat: finish x2

---
+++ PROYECTO 4/proyecto4/datos.js
@@ -0,0 +1,66 @@
+export const datosCV = {
+  nombre: 'Iker Pardo',
+  puesto: 'Desarrollador Full Stack',
+  email: 'ikerpardo53@gmail.com',
+  linkedin: 'www.linkedin.com/in/iker-pardo-masache',
+  foto: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQUzNq4pcWb3OS95q1_vvui0zuA4YZqQQaMCVljbpeMerkNPO0w',
+  lugarResidencia: 'Madrid, España',
+  biografia:
+    'Soy un desarrollador back-end comprometido, con un sólido dominio del diseño de código y una habilidad destacada en la resolución de problemas complejos en el sector informático. Mi pasión por la informática se refleja en mi enfoque organizado y en mi constante búsqueda de la excelencia en cada tarea que emprendo. Estoy entusiasmado por unirme a un equipo de trabajo donde pueda contribuir con mis conocimientos y experiencia, y seguir creciendo profesionalmente.',
+  educacion: [
+    {
+      grado: 'Desarrollo de Aplicaciones Multiplataforma',
+      centro: 'IES El Cañaveral',
+      fechaFin: 2021
+    },
+    {
+      grado: 'Animacion 3D, Juegos y Entornos Interactivos',
+      centro: 'CIFD Ignacio Ellacuria',
+      fechaFin: 2024
+    },
+    {
+      grado: 'Desarrollador Full Stack',
+      centro: 'ThePower MBA',
+      fechaFin: 'Actualidad'
+    }
+  ],
+  experiencia: [
+    {
+      puesto: 'Desarrollador Front-End',
+      empresa: 'NBS Spain',
+      fechaInicio: '2021',
+      fechaFin: '2021'
+    },
+    {
+      puesto: 'Desarrollador Back-End',
+      empresa: 'Mixreal',
+      fechaInicio: '2023',
+      fechaFin: '2024'
+    }
+  ],
+  competencias: [
+    'C#',
+    'HTML5',
+    'CSS3',
+    'JavaScript',
+    'AWS',
+    'MySQL',
+    'Node.js',
+    'MongoDB',
+    'Git'
+  ],
+  proyectos: [
+    {
+      titulo: 'Desarrollo de un Videojuego en 3D',
+      descripcion:
+        'Creé un videojuego en 3D de disparos en primera persona utilizando la plataforma Unity. El desarrollo incluyó la construcción de entornos de juego detallados, la planificación y diseño de niveles, la programación de la inteligencia artificial para los enemigos y la implementación de mecánicas de juego como el manejo de armas y la detección de colisiones. Utilicé C# para escribir los scripts y optimicé el rendimiento del juego para asegurar una experiencia fluida y envolvente para los jugadores',
+      link: 'http://github.com/#'
+    },
+    {
+      titulo: 'Modelado 3D de una Vivienda con Autodesk Maya',
+      descripcion:
+        'Llevé a cabo el modelado 3D de una vivienda de dos pisos utilizando Autodesk Maya. Este proyecto abarcó desde la fase de diseño y conceptualización arquitectónica hasta la creación de texturas y la configuración de la iluminación. Me enfoqué en los detalles tanto del interior como del exterior, incluyendo mobiliario y paisajismo. El modelo final fue renderizado para presentaciones arquitectónicas, ofreciendo una visualización precisa y realista del diseño',
+      link: 'http://github.com/#'
+    }
+  ]
+}
